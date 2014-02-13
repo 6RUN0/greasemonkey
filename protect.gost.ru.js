177dd0cc8bf3e3b012d9a4c91a18bc62fcb3c1f7
boris_t <boris@talovikov.ru>
2014-02-13 13:04:53 +0600

    оптимизировал и профиксил баг http://userscripts.org/topics/66015#posts-309144

--- protect.gost.ru.js
+++ protect.gost.ru.js
@@ -1,27 +1,33 @@
 // ==UserScript==
-// @author Chamie (chamie.habrahabr.ru)
-// @name GOST free view
-// @include http://protect.gost.ru/v.aspx*
-// @description Открывает "защищённые" документы ГОСТа одной страницей, позволяя сохранить их.
-// @version 1.0.2
+// @author       Chamie (chamie.habrahabr.ru)
+// @name         GOST free view
+// @include      http://protect.gost.ru/v.aspx?control=8*
+// @description  Открывает "защищённые" документы ГОСТа одной страницей, позволяя сохранить их.
+// @version      1.0.2p1
+// @grant        none
+// @run-at       document-end
 // ==/UserScript==
+
 function fixGreed(){
-	td=document.getElementsByClassName("download")[0].getElementsByTagName("table")[1].getElementsByTagName("td")[1];
-	td.innerHTML+="<button id='showall'>Показать все</button>";
-	document.getElementById('showall').addEventListener("click",showAll,true);
-	debugger
+  td = document.getElementsByClassName('download')[0].getElementsByTagName('table')[1].getElementsByTagName('td')[1];
+  links = td.getElementsByTagName('a');
+  td.innerHTML += '<button id="showall">Показать все</button>';
+  document.getElementById('showall').addEventListener('click', function(){showAll(links);}, true);
 }
-function showAll(){
-	td=document.getElementsByClassName("download")[0].getElementsByTagName("table")[1].getElementsByTagName("td")[1];
-	lnks=td.getElementsByTagName("a");
-	wnd = window.open();
-	wnd.focus();
-	wnd.document.open();
-	txt="<html><head><title>Все страницы</title></head><body>";
-	for(i=0;i<lnks.length;i++)
-		txt+="<img src='http://protect.gost.ru/image.ashx?page="+lnks[i].href.split("pageK=")[1]+"'><br>";
-	txt+="</body></html>";
-	wnd.document.write(txt);
-	wnd.document.close();
+
+function showAll(links){
+  wnd = window.open();
+  wnd.focus();
+  wnd.document.open();
+  txt = '<html><head><title>Все страницы</title></head><body style="text-align: center;">';
+  for(i=0; i<links.length; i++) {
+    if(links[i].innerHTML != '') {
+      txt += '<img src="http://protect.gost.ru/image.ashx?page='+links[i].href.split('pageK=')[1]+'"><br>';
+    }
+  }
+  txt += '</body></html>';
+  wnd.document.write(txt);
+  wnd.document.close();
 }
+
 fixGreed();
